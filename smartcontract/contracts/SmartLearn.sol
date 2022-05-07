5118541b4dc5f3c06243a4118ff0a09db00ef800
iman abbasizadeh <imanabc007@gmail.com>
2022-05-07 15:45:25 +0430

    Add TaskFactory contract and make changes to SmartLearn contract

--- smartcontract/contracts/SmartLearn.sol
+++ smartcontract/contracts/SmartLearn.sol
@@ -2,212 +2,50 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./Financable.sol";
+import "./TaskFactory.sol";
 
-contract SmartLearn is Ownable, Financable{
+contract SmartLearn is Ownable, TaskFactory {
   
-  /*
-   * Constant variables
-   */
+ // Get user's all tasks
+  function getTasks() public view returns (Task[] memory) {
+    return _getTasks();
+  }
   
-  // Punishment period in seconds
-  uint constant PUNISHMENT_TIME = 30 days;
-
-  /*
-   * State variables
-   */
-
-  struct Task {
-    string description;
-    uint createdAt;
-    uint value;
-    uint dueDate;
-    bool completed;
-    bool cleared;
-  }
-
-  // Tasks
-  mapping (address => Task[]) public tasks;
-
-  /* 
-   * Events
-   */
-
-  // Task added event
-  event LogAdd (address userAddress, uint taskID); // TODO
-
-  /* 
-   * Modifiers
-   */
-
-  modifier validDueDate (uint _date) {
-    require(_date > block.timestamp);
-    _;
-  }
-
-  // If task has not any prize locked in it
-  modifier notPrized (uint _id) {
-    require(!hasPrize(_id));
-    _;
-  }
-
-  modifier completed (uint _id) {
-    require(getTask(_id).completed);
-    _;
-  }
-
-  modifier incomplete (uint _id) {
-    require(!getTask(_id).completed);
-    _;
-  }
-
-  modifier dueIsSet (uint _id) {
-    require(getTask(_id).dueDate != 0);
-    _;
-  }
-
-
-
-  /* 
-   * Functions
-   */
-
-  // Add task
+  // Add a new task
   function add(string memory _description, uint _dueDate) public {
-    Task memory task = Task({
-      description: _description,
-      createdAt: block.timestamp,
-      value: 0,
-      dueDate: _dueDate,
-      completed: false,
-      cleared: true
-    });
-    tasks[msg.sender].push(task);
+    _add(_description, _dueDate);
   }
 
-
-  // Update task
-  function update(uint _id, string memory _description, uint _dueDate)
-  public notPrized(_id) {
-    Task memory task = tasks[msg.sender][_id];
-    task.description = _description;
-    task.dueDate = _dueDate;
-    replace(_id, task);
+  // Set a task as completed
+  function setComplete(uint _id) public {
+    _setComplete(_id);
   }
 
-  // Remove task
-  function remove(uint _id) public notPrized(_id) {
-    require(_id < tasks[msg.sender].length);
-
-    tasks[msg.sender][_id] = tasks[msg.sender][tasks[msg.sender].length - 1];
-    tasks[msg.sender].pop();
-  }
-  
-
-  // Set complete
-  function setComplete(uint _id) public incomplete(_id) {
-    Task memory task = getTask(_id);
-    if (!hasPrize(_id) || !isExpired(_id)) {
-      task.completed = true;
-      replace(_id, task);
-    }
-    if (hasPrize(_id) && !task.cleared) {
-      if (!isPunishing(_id)) {
-        task.cleared = true;
-        task.completed = true;
-        replace(_id, task);
-        addPrize(msg.sender, task.value);
-      }
-    }
-  }
-
-  // Set incomplete
-  function setIncomplete(uint _id) public completed(_id) notPrized(_id) {
-    Task memory task = getTask(_id);
-    task.completed = false;
-    replace(_id, task);
-  }
-
-  // Has prize locked in it
-  function hasPrize(uint _id) internal view returns (bool) {
-    Task memory task = getTask(_id);
-    if (task.value == 0)
-      return false;
-    return !task.cleared;
+  // Set prize for a task
+  function setPrize(uint _id) public payable {
+    _setPrize(_id);
   }
 
   // Clear a task
   function clear(uint _id) public returns (bool) {
-    Task memory task = getTask(_id);
-    if(hasPrize(_id) || isPunishing(_id)) {
-      return false;
-    }
-    if(isPunishmentOver(_id)) {
-      task.cleared = true;
-      payUser(task.value);
-      replace(_id, task);
-      return true;
-    }
-    return false;
+    return _clear(_id);
   }
 
-  // Set prize
-  function setPrize(uint _id) public payable dueIsSet(_id) {
-    Task memory task = getTask(_id);
-    require(task.value == 0);
-    require(!isExpired(_id));
-    require(task.cleared);
+  /**
+   *pause() function is implemented to stop all deposits to the 
+   *contract so that new users wouldn't loss their coins 
+   *if there are any bugs in contract.
+   */
 
-    task.value = msg.value;
-    task.cleared = false;
-
-    replace(_id, task);
+    // Pause contract deposits
+  function pause() public onlyOwner() {
+    _pause();
   }
 
-  // Is in punishment period
-  function isPunishing(uint _id) public view returns (bool) {
-    if (!hasPrize(_id)) {
-      return false;
-    }
-    if (!!isExpired(_id)) {
-      return false;
-    }
-    return !isPunishmentOver(_id);
-  }
-
-  // Is punishment period over
-  function isPunishmentOver(uint _id) private view returns (bool){
-    Task memory task = getTask(_id);
-    if (block.timestamp > task.dueDate + PUNISHMENT_TIME) {
-      return true;
-    }
-    return false;
-  }
-
-  // Is task overdue
-  function isExpired(uint _id) internal view returns (bool) {
-    Task memory task = getTask(_id);
-    if (task.dueDate == 0) {
-      return false;
-    }
-    return (task.dueDate < block.timestamp);
-  }
-
-  // Get current user tasks
-  function getTasks() public view returns(Task[] memory) {
-    return tasks[msg.sender];
-  }
-
-  // Get task
-  function getTask(uint _id) public view returns(Task memory) {
-    require(tasks[msg.sender].length>_id);
-    return tasks[msg.sender][_id];
-  }
-
-
-  // Replace task
-  function replace(uint _id, Task memory _task) internal {
-    tasks[msg.sender][_id] = _task;
+  // Unpause contract deposits
+  function unpause() public onlyOwner() {
+    _unpause();
   }
 
 }
+  
---
+++ smartcontract/contracts/TaskFactory.sol
@@ -0,0 +1,212 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "./Financable.sol";
+
+contract TaskFactory is Financable, Pausable {
+
+  /**
+   * Constant variables
+   */
+  
+  // Punishment period
+  uint constant PUNISHMENT_TIME = 30 days;
+
+
+  /**
+   * State variables
+   */
+
+  struct Task {
+    string description;
+    uint createdAt;
+    uint value;
+    uint dueDate;
+    bool completed;
+    bool cleared;
+  }
+
+  mapping (address => Task[]) public tasks;
+
+
+  /** 
+   * Events
+   */
+
+  event LogAdd (address userAddress, uint taskID);
+
+
+  /**
+   * Modifiers
+   */
+
+  modifier validDueDate (uint _date) {
+    require(_date > block.timestamp);
+    _;
+  }
+
+  // If task has not any prize locked in it
+  modifier notPrized (uint _id) {
+    require(!hasPrize(_id));
+    _;
+  }
+
+  modifier completed (uint _id) {
+    require(getTask(_id).completed);
+    _;
+  }
+
+  modifier incomplete (uint _id) {
+    require(!getTask(_id).completed);
+    _;
+  }
+
+  modifier dueIsSet (uint _id) {
+    require(getTask(_id).dueDate != 0);
+    _;
+  }
+
+  /**
+   * Functions
+   */
+
+  // Add task
+  function _add(string memory _description, uint _dueDate) internal {
+    Task memory task = Task({
+      description: _description,
+      createdAt: block.timestamp,
+      value: 0,
+      dueDate: _dueDate,
+      completed: false,
+      cleared: true
+    });
+    tasks[msg.sender].push(task);
+  }
+
+
+  // Update task
+  function _update(uint _id, string memory _description, uint _dueDate)
+  internal notPrized(_id) {
+    Task memory task = tasks[msg.sender][_id];
+    task.description = _description;
+    task.dueDate = _dueDate;
+    replace(_id, task);
+  }
+
+  // Remove task
+  function _remove(uint _id) internal notPrized(_id) {
+    require(_id < tasks[msg.sender].length);
+
+    tasks[msg.sender][_id] = tasks[msg.sender][tasks[msg.sender].length - 1];
+    tasks[msg.sender].pop();
+  }
+  
+
+  // Set complete
+  function _setComplete(uint _id) internal incomplete(_id) {
+    Task memory task = getTask(_id);
+    if (!hasPrize(_id) || !isExpired(_id)) {
+      task.completed = true;
+      replace(_id, task);
+    }
+    if (hasPrize(_id) && !task.cleared) {
+      if (!isUnderPunishment(_id)) {
+        task.cleared = true;
+        task.completed = true;
+        replace(_id, task);
+        addPrize(msg.sender, task.value);
+      }
+    }
+  }
+
+  // Set incomplete
+  function _setIncomplete(uint _id) internal completed(_id) notPrized(_id) {
+    Task memory task = getTask(_id);
+    task.completed = false;
+    replace(_id, task);
+  }
+
+  // Has prize locked in it
+  function hasPrize(uint _id) private view returns (bool) {
+    Task memory task = getTask(_id);
+    if (task.value == 0)
+      return false;
+    return !task.cleared;
+  }
+
+  // Clear a task
+  function _clear(uint _id) internal returns (bool) {
+    Task memory task = getTask(_id);
+    if(hasPrize(_id) || isUnderPunishment(_id)) {
+      return false;
+    }
+    if(isPunishmentOver(_id)) {
+      task.cleared = true;
+      payUser(task.value);
+      replace(_id, task);
+      return true;
+    }
+    return false;
+  }
+
+  // Set prize
+  function _setPrize(uint _id) internal dueIsSet(_id) whenNotPaused() {
+    Task memory task = getTask(_id);
+    require(task.value == 0);
+    require(!isExpired(_id));
+    require(task.cleared);
+
+    task.value = msg.value;
+    task.cleared = false;
+
+    replace(_id, task);
+  }
+
+  // Is under punishment period
+  function isUnderPunishment(uint _id) private view returns (bool) {
+    if (!hasPrize(_id)) {
+      return false;
+    }
+    if (!!isExpired(_id)) {
+      return false;
+    }
+    return !isPunishmentOver(_id);
+  }
+
+  // Is punishment period over
+  function isPunishmentOver(uint _id) private view returns (bool){
+    Task memory task = getTask(_id);
+    if (block.timestamp > task.dueDate + PUNISHMENT_TIME) {
+      return true;
+    }
+    return false;
+  }
+
+  // Is task expired
+  function isExpired(uint _id) private view returns (bool) {
+    Task memory task = getTask(_id);
+    if (task.dueDate == 0) {
+      return false;
+    }
+    return (task.dueDate < block.timestamp);
+  }
+
+  // Get current user tasks
+  function _getTasks() internal view returns(Task[] memory) {
+    return tasks[msg.sender];
+  }
+
+  // Get task
+  function getTask(uint _id) private view returns(Task memory) {
+    require(tasks[msg.sender].length>_id);
+    return tasks[msg.sender][_id];
+  }
+
+
+  // Replace task
+  function replace(uint _id, Task memory _task) private {
+    tasks[msg.sender][_id] = _task;
+  }
+
+}
